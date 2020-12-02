a5a7134693c6b2c80eee611a9d09df8298fd0196
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-12-03 04:19:20 +0800

    import card deck dan bootstrap pada src app js

--- src/App.js
+++ src/App.js
@@ -8,45 +8,35 @@ function App() {
   return (
     <div>
       <CardDeck>
-  <Card bg="secondary">
+  <Card bg="secondary" text="white">
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
+      <Card.Title>Cases</Card.Title>
+      <Card.Text>100</Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small >Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card>
+  <Card bg="danger" text={"white"}>
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
+      <Card.Title>Deaths</Card.Title>
+      <Card.Text>0</Card.Text>
     </Card.Body>
     <Card.Footer>
+      <small>Last update 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card>
-    <Card/>
+  <Card bg="success" text={"white"}>
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
+      <Card.Title>Recovered</Card.Title>
+      <Card.Text>99</Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small>Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
 </CardDeck>
-    </div>
+</div>
   );
 }
 
