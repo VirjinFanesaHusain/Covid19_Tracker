82b1266e0a8763c19c650eaf88a76b977e7d1565
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-12-03 06:20:26 +0800

    Menggunakan axios utk akses link cases,deaths,recovered

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 
 function App() {
+  const [latest, setLatest] = useState("");
+
+  useEffect(() => {
+    axios
+    .get("https://corona.lmao.ninja/v2/all")
+    .then(res => {
+      setLatest(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
   return (
     <div>
       <CardDeck>
-  <Card bg="secondary" text="white" className="text-center">
+  <Card 
+    bg="secondary" 
+    text="white" 
+    className="text-center" 
+    style={{margin: "10px"}}>
+
     <Card.Body>
       <Card.Title>Cases</Card.Title>
-      <Card.Text>100</Card.Text>
+      <Card.Text>{latest.cases}</Card.Text>
     </Card.Body>
     <Card.Footer>
       <small >Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card bg="danger" text={"white"} className="text-center">
+  <Card bg="danger" 
+    text={"white"} 
+    className="text-center"  
+    style={{margin: "10px"}}>
+    
     <Card.Body>
       <Card.Title>Deaths</Card.Title>
-      <Card.Text>0</Card.Text>
+      <Card.Text>{latest.deaths}</Card.Text>
     </Card.Body>
     <Card.Footer>
       <small>Last update 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card bg="success" text={"white"} className="text-center">
+  <Card bg="success" 
+    text={"white"} 
+    className="text-center"  
+    style={{margin: "10px"}}>
     <Card.Body>
       <Card.Title>Recovered</Card.Title>
-      <Card.Text>99</Card.Text>
+      <Card.Text>{latest.recovered}</Card.Text>
     </Card.Body>
     <Card.Footer>
       <small>Last updated 3 mins ago</small>
